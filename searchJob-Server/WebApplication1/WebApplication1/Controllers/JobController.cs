ae0e3564d4a0cdd4c6566fd1e45f568e0a763117
TamarSalomon <tamar31780@gmail.com>
2024-05-07 18:26:37 +0300

    Code improvements

--- searchJob-Server/WebApplication1/WebApplication1/Controllers/JobController.cs
+++ searchJob-Server/WebApplication1/WebApplication1/Controllers/JobController.cs
@@ -18,7 +18,20 @@ namespace WebApplication1.Controllers
             new Job(JobField.QA, "Matrix", 45, "BA",  false, Area.SOUTH),
             new Job(JobField.PROGRAMMER, "FaceBook",50, "Maat" ,  false, Area.CENTER),
             new Job(JobField.PROGRAMMER,"Google", 35, "BA", true ,Area.CENTER),
-             new Job(JobField.ANALYST, "Cheak Point" , 45, "MAAT", false, Area.NORTH)
+             new Job(JobField.ANALYST, "Cheak Point" , 45, "MAAT", false, Area.NORTH),
+           new Job(JobField.PROGRAMMER, "UCC", 36, "Maat", true, Area.SOUTH),
+           new Job(JobField.QA, "Cellebrite", 32, "BA", true, Area.CENTER),
+
+            new Job(JobField.PROGRAMMER, "NESS", 24, "Maat", true, Area.SOUTH),
+            new Job(JobField.ANALYST, "CODE OASIS", 22, "BA", true, Area.CENTER),
+
+            new Job(JobField.PROGRAMMER, "Software AG", 31, "Maat", true, Area.NORTH),
+            new Job(JobField.ANALYST, " FatFish", 30, "BA", true, Area.SOUTH),
+
+            new Job(JobField.QA, "GRTH", 38, "BA", true, Area.NORTH),
+            new Job(JobField.QA, "EXTREME", 35, "Maat", true, Area.CENTER),
+
+
         };
 
         [HttpGet("GetAllJobs")]
